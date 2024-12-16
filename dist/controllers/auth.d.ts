aaf5265bd813b3bfde9811fd3d828079d7a23731
Sébastien Lau <sebastien.lau2021@gmail.com>
2024-12-16 18:24:51 +0100

    try config

--- dist/controllers/auth.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-declare const _default: {
-    signup: (req: Request, res: Response, next: NextFunction) => Promise<void>;
-    login: (req: Request, res: Response, next: NextFunction) => Promise<void>;
-    getAllUser: (req: Request, res: Response, next: NextFunction) => Promise<void>;
-};
-export default _default;
--- dist/middlewares/authorizeRequest.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-interface AuthRequest extends Request {
-    auth?: {
-        userId: string;
-    };
-}
-declare const authorizeRequest: (req: AuthRequest, res: Response, next: NextFunction) => void;
-export default authorizeRequest;
